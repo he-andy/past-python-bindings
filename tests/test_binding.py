d5f028d10fb70d1d8069f85574eaf2429f59582b
Niansong Zhang <nz264@cornell.edu>
2024-10-15 21:05:59 -0400

    Revert changes in test_binding

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -1,16 +1,14 @@
 import pytest
 import past
 
-
-
 def test_gemm():
     prog_a = "gemm/vanilla.c"
     prog_b = "gemm/acc-inter.c"
     out_var = "C"
-    ret = past.verify(prog_a, prog_b, out_var)
-    print("return value: ", ret)
+    is_equivalent = past.verify(prog_a, prog_b, out_var)
+    assert is_equivalent
+
 
 if __name__ == "__main__":
-    # pytest.main([__file__])
-    # print("All tests passed!")
-    test_gemm()
+    pytest.main([__file__])
+    print("All tests passed!")
